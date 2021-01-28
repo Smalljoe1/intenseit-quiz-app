2fbe7fb0bedbe403d4b2fb4e661cd8ab677f5b6c
Smalljoe1 <nongujoseph@yahoo.com>
2021-01-28 13:38:57 +0100

    Upload files

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import React  from 'react';
+import QuizNavigator from './app/navigation/QuizNavigator'
+
+
+const App = () => {
+    
+     return (
+       
+    <>
+       <QuizNavigator />
+     </>
+  );
+};
+
+export default App;
